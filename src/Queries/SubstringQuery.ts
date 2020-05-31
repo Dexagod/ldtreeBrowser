3c25c3b89054f2d84fc66a73da73898fb6cb84ad
Ruben Dedecker <dedecker.ruben@gmail.com>
2020-05-31 20:16:16 +0200

    WIP

--- src/Queries/SubstringQuery.ts
+++ src/Queries/SubstringQuery.ts
@@ -1,6 +1,9 @@
 import { Query } from './Query';
 import { Normalizer } from './Normalizer';
 import TinyQueue from "tinyqueue"
+import asyncPool from "tiny-async-pool";
+import { rejects } from 'assert';
+
 
 const DEFAULTCOUNT=100000
 
@@ -19,7 +22,45 @@ export class SubstringQuery extends Query {
   relationQueue = new TinyQueue([], function(a:any, b:any) { return b.count - a.count});
   rootNodeItems = null;
 
-  async followChildRelations(nodeId : any, nodesMetadata : any, value : any, followedValue : any, level : any) : Promise<Array<any>> {
+  constructor(){
+    super();
+    this.checkRelationFunction = this.checkRelationFunction.bind(this);
+  }
+
+  async query(collectionId : any, value : any, session : any = null, maxamount:number = Infinity) : Promise<any>{
+    // let results = await this.processId(collectionId)
+    let results = await this.addTask(this.taskqueue, this.processIdTask, collectionId, this);
+    if (results !== undefined && results !== null){
+      for (let collection of results.collections){
+        if (collection.id === collectionId){
+          for (let viewRootNodeId of collection.views){
+            await this.recursiveQueryNode(viewRootNodeId, value, this.getInitialSearchValue(), 0)
+          }
+        }
+      } 
+    }
+  }
+
+  // async function main(){
+  //   var timings = [500, 800, 200, 400]
+  //   const timeout = (i: number) => new Promise(resolve => setTimeout(() => {console.log("done", i); timings.push(3000); resolve(i)}, i));
+  //   const results = await asyncPool(2, timings, timeout);
+  // }
+
+  checkRelationFunction = async function (ro: RelationObject) { !this.terminated && await this.recursiveQueryNode(ro.relationnode, ro.value, ro.relationvalue, ro.level) }
+
+
+  async recursiveQueryNode(currentNodeId : any, value : any, followedValue : any, level : any) : Promise<any> {
+    if (this.terminated) return;
+    let results = await this.addTask(this.taskqueue, this.processIdTask, currentNodeId, this);
+    if (this.terminated || results === undefined || results === null) return;
+
+    this.handleEmittingMembers(results, currentNodeId, followedValue, level)
+    return await this.followChildRelations(currentNodeId, results.nodes, value, followedValue, level + 1)
+  }
+
+
+  async followChildRelations(nodeId : any, nodesMetadata : any, value : any, followedValue : any, level : any) : Promise<any> {
     // let runningQueries = new Array()
     for (let node of nodesMetadata){
       if (node.id === nodeId){  
@@ -28,8 +69,6 @@ export class SubstringQuery extends Query {
           if (relation.type === TREE+"SubstringRelation" || relation.type === TREE+"EqualThanRelation"){
             let count = this.checkValue(value, relation.value, (relation.remainingItems / (this.rootNodeItems||1)) || 0 , relation.type)
             if(count > 0){
-              // runningQueries.push({count: count, relation: await this.followChildWithValue(relation.node, relation.value, value, level)})
-              // console.log("pushing", "followed", followedValue, "relation", "'" + relation.value + "'", count)
               console.log("pushing", relation.value)
               this.relationQueue.push({count: count, relationnode: relation.node, relationvalue: relation.value, value: value, level: level})
             }
@@ -37,28 +76,10 @@ export class SubstringQuery extends Query {
         }
       }
     }
-
-    let returnlist = new Array();
-    let runningQueries = [];
-    while (this.relationQueue.length){
-      let relationData = this.relationQueue.pop()
-      let queryExecution = await this.followChildWithValue(relationData.relationnode, relationData.relationvalue, relationData.value, relationData.level)
-      runningQueries.push(queryExecution);
-    }
-    await Promise.all(runningQueries);
-    for (let list of await runningQueries){
-      returnlist = returnlist.concat(await list)
-    }
-    return returnlist
   }
 
   async followChildWithValue(relationNodeId: any, relationValue: any, searchValue: any, level: any) : Promise<Array<any>> {
-    let count = this.checkValue(searchValue, relationValue)
-    if(count > 0 && !this.terminated){
-      console.log("FOLLOWING", "'" +relationValue+"'", count)
-      return await this.recursiveQueryNode(relationNodeId, searchValue, relationValue, level)
-    }
-    return [];
+    throw new Error("Method not implemented")
   }
 
   getInitialSearchValue() : any{
@@ -119,4 +140,12 @@ export class SubstringQuery extends Query {
     return results;
   }
 
+}
+
+interface RelationObject {
+  count: number, 
+  relationnode: string, 
+  relationvalue: any, 
+  value: any, 
+  level: number
 }
